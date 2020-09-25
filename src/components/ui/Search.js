4530befe83ca9eff509225d9f71b875da772b9f5
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-25 00:49:25 -0400

    Added functionality to the search component, so that the state can be manipluated via the useState hook and an onChange event is recognized.

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 
 const Search = () => {
+    const [text, setText] = useState("")
+
     return (
         <section className="search">
             <form>
-                <input type="text" className="form-control" placeholder="Enter name of Breaking Bad character" autoFocus />
+                <input type="text" 
+                className="form-control" 
+                placeholder="Enter name of Breaking Bad character"
+                value={text} 
+                onChange={(event) => setText(event.target.value)}
+                autoFocus />
             </form>
         </section>
     );
