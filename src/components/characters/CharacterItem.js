7cf7ade33c252dcb3162ce52bfd2e43d4c52cb42
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-25 00:29:46 -0400

    Created the card back. Added character information to the back of the card. Animation/Application is running as expected.

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -8,6 +8,26 @@ const CharacterItem = ({ character }) => {
                     <div className="card-front">
                         <img src={character.img} alt="" />
                     </div>
+                    <div className="card-back">
+                        <h1>{character.name}</h1>
+                        <ul>
+                            <li>
+                                <strong>Nickname:</strong> {character.name}
+                            </li>
+                            <li>
+                                <strong>Birthday:</strong> {character.birthday}
+                            </li>
+                            <li>
+                                <strong>Occupation:</strong> {character.occupation}
+                            </li>
+                            <li>
+                                <strong>Status:</strong> {character.status}
+                            </li>
+                            <li>
+                                <strong>Portrayed By:</strong> {character.portrayed}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
